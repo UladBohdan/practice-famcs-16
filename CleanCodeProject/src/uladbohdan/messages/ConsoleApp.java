07b29f10a35ae62b78a0c349ea20149185bf5cba
UladBohdan <vlbohdan@gmail.com>
2016-02-12 19:07:45 +0300

    JSON Console App: logfile support added

--- CleanCodeProject/src/uladbohdan/messages/ConsoleApp.java
+++ CleanCodeProject/src/uladbohdan/messages/ConsoleApp.java
@@ -15,6 +15,13 @@ public class ConsoleApp {
     public ConsoleApp() {
         data = new ArrayList<>();
         try {
+            logfile = new FileWriter("logfile");
+            log("NEW SESSION");
+        } catch (IOException e) {
+            System.out.println("Sorry, logfile won't be formatted.");
+            System.out.println();
+        }
+        try {
             System.out.println("Console App for working with messages in JSON format.");
             System.out.println("Type " + COMM + "help" + END + " for list of commands; " +
                     COMM + "quit" + END + " to quit.");
@@ -23,6 +30,10 @@ public class ConsoleApp {
                 System.out.print("$ ");
                 if (!handleCommand(in.nextLine()))
                     break;
+            }
+            if (logfile != null) {
+                log("END OF SESSION");
+                logfile.close();
             }
         } catch (Exception e) {
             System.out.println("Something went wrong:(");
@@ -39,6 +50,7 @@ public class ConsoleApp {
         String[] command = input.split(" ");
         switch (command[0]) {
             case "help":
+                log("HELP queried");
                 System.out.println("List of available commands:");
                 System.out.println(COMM + "load [file]" + END +
                         " - load messages from file. Previous data will be lost");
@@ -93,10 +105,12 @@ public class ConsoleApp {
             data.clear();
             Collections.addAll(data, temp);
             System.out.println("Successfully read " + temp.length + " messages.");
+            log("LOAD " + command[1] + " successfully read " + temp.length + " messages.");
             file = command[1];
             reader.close();
         } catch (Exception e) {
             System.out.println(RED + "FAILED loading from file" + END);
+            log("LOAD " + command[1] + " loading failed.");
         }
     }
 
@@ -116,53 +130,68 @@ public class ConsoleApp {
             msg.deleteCharAt(msg.length()-1);
             data.add(new Message(command[1], msg.toString()));
         }
-        System.out.println("Successfully added: " + data.get(data.size() - 1).getFormattedMessage());
+        Message last = data.get(data.size() - 1);
+        if (last.getMessage().length() > 140)
+            System.out.println(RED + "WARNING: long message (more than 140 symbols)." + END);
+        System.out.println("Successfully added: " + last.getFormattedMessage());
+        log("ADD " + last.getId() + " " + last.getAuthor() + " " + last.getMessage());
     }
 
     private void showMessages(String[] command) {
-        Collections.sort(data);
-        if (command.length == 1) {
-            System.out.println("FULL LIST OF MESSAGES:");
-            for (Message i : data)
-                System.out.println(i.toString());
-        } else if (command.length == 2 && command[1].equals("-f")) {
-            System.out.println("FORMATTED LIST OF MESSAGES:");
-            for (Message i : data)
-                System.out.println(i.getFormattedMessage());
-        } else {
-            LocalDateTime timeBegin, timeEnd;
-            boolean isF;
-            if (command[1].equals("-f")) {
-                isF = true;
-                timeBegin = LocalDateTime.parse(command[2], DateTimeFormatter.ISO_LOCAL_DATE_TIME);
-                if (command.length > 3)
-                    timeEnd = LocalDateTime.parse(command[3], DateTimeFormatter.ISO_LOCAL_DATE_TIME);
-                else
-                    timeEnd = LocalDateTime.now().plusMinutes(2);
+        try {
+            Collections.sort(data);
+            if (command.length == 1) {
+                System.out.println("FULL LIST OF MESSAGES:");
+                log("QUERY list");
+                for (Message i : data)
+                    System.out.println(i.toString());
+            } else if (command.length == 2 && command[1].equals("-f")) {
+                System.out.println("FORMATTED LIST OF MESSAGES:");
+                log("QUERY formatted list");
+                for (Message i : data)
+                    System.out.println(i.getFormattedMessage());
             } else {
-                isF = false;
-                timeBegin = LocalDateTime.parse(command[1], DateTimeFormatter.ISO_LOCAL_DATE_TIME);
-                if (command.length > 2)
-                    timeEnd = LocalDateTime.parse(command[2], DateTimeFormatter.ISO_LOCAL_DATE_TIME);
-                else
-                    timeEnd = LocalDateTime.now().plusMinutes(2);
-            }
-            if (isF)
-                System.out.println("FORMATTED LIST OF MESSAGES from " + timeBegin + " to " + timeEnd);
-            else
-                System.out.println("FULL LIST OF MESSAGES from " + timeBegin + " to " + timeEnd);
-            for (Message i : data)
-                if (i.getTime().compareTo(timeBegin) > 0 &&
-                        i.getTime().compareTo(timeEnd) < 0)
-                    if (isF)
-                        System.out.println(i.getFormattedMessage());
+                LocalDateTime timeBegin, timeEnd;
+                boolean isF;
+                if (command[1].equals("-f")) {
+                    isF = true;
+                    timeBegin = LocalDateTime.parse(command[2], DateTimeFormatter.ISO_LOCAL_DATE_TIME);
+                    if (command.length > 3)
+                        timeEnd = LocalDateTime.parse(command[3], DateTimeFormatter.ISO_LOCAL_DATE_TIME);
                     else
-                        System.out.println(i.toString());
+                        timeEnd = LocalDateTime.now().plusMinutes(2);
+                } else {
+                    isF = false;
+                    timeBegin = LocalDateTime.parse(command[1], DateTimeFormatter.ISO_LOCAL_DATE_TIME);
+                    if (command.length > 2)
+                        timeEnd = LocalDateTime.parse(command[2], DateTimeFormatter.ISO_LOCAL_DATE_TIME);
+                    else
+                        timeEnd = LocalDateTime.now().plusMinutes(2);
+                }
+                if (isF) {
+                    System.out.println("FORMATTED LIST OF MESSAGES from " + timeBegin + " to " + timeEnd);
+                    log("QUERY formatted list: " + timeBegin + " to " + timeEnd);
+                } else {
+                    System.out.println("FULL LIST OF MESSAGES from " + timeBegin + " to " + timeEnd);
+                    log("QUERY list: " + timeBegin + " to " + timeEnd);
+                }
+                for (Message i : data)
+                    if (i.getTime().compareTo(timeBegin) > 0 &&
+                            i.getTime().compareTo(timeEnd) < 0)
+                        if (isF)
+                            System.out.println(i.getFormattedMessage());
+                        else
+                            System.out.println(i.toString());
+            }
+        } catch (Exception e) {
+            System.out.println("Failed on your query. Try another one.");
+            log("QUERY failed");
         }
     }
 
     private void deleteMessage(String[] command) {
         if (command.length == 1) {
+            log("DELETE failed: lack of arguments");
             System.out.println(RED + "ERROR: not enough arguments. ID is required to delete a message" + END);
             return;
         }
@@ -174,10 +203,13 @@ public class ConsoleApp {
                 isRemoved = true;
             }
         }
-        if (isRemoved)
+        if (isRemoved) {
             System.out.println("Successfully removed by id: " + id);
-        else
+            log("DELETE successfully by id: " + id);
+        } else {
             System.out.println("Message not found. Nothing removed.");
+            log("DELETE failed: if not found");
+        }
     }
 
     private void saveMessagesToJSONFile(String[] command) {
@@ -185,6 +217,7 @@ public class ConsoleApp {
         if (command.length == 1) {
             if (file.equals("null")) {
                 System.out.println(RED + "ERROR: not enough arguments." + END);
+                log("SAVE failed: lack of arguments");
                 return;
             } else {
                 System.out.println("Will be saved to " + file);
@@ -198,7 +231,9 @@ public class ConsoleApp {
             gson.toJson(data, out);
             out.close();
             System.out.println("Successfully saved to " + saveTo);
+            log("SAVE successfully to " + saveTo + ", " + data.size() + " messages");
         } catch (Exception e) {
+            log("SAVE failed");
             System.out.println(RED + "FAILED saving to file" + END);
         }
     }
@@ -206,54 +241,77 @@ public class ConsoleApp {
     private void searchMessages(String[] command) {
         if (command.length < 3) {
             System.out.println(RED + "ERROR: not enough arguments. Parameters are required" + END);
+            log("SEARCH failed: lack of arguments.");
             return;
         }
         try {
-            boolean exists;
+            int counter;
             switch (command[1]) {
                 case "-author":
                     System.out.println("BY AUTHOR: " + command[2]);
-                    exists = false;
+                    counter = 0;
                     for (Message i : data) {
                         if (i.getAuthor().equals(command[2])) {
                             System.out.println(i.getFormattedMessage());
-                            exists = true;
+                            counter++;
                         }
                     }
-                    if (!exists)
+                    if (counter == 0) {
                         System.out.println("nothing found");
+                        log("SEARCH by author: " + command[2] + ", nothing found");
+                    } else {
+                        log("SEARCH by author: " + command[2] + ", found: " + counter);
+                    }
                     break;
                 case "-keyword":
                     System.out.println("BY KEYWORD: " + command[2]);
-                    exists = false;
+                    counter = 0;
                     for (Message i : data) {
                         if (i.getMessage().contains(command[2])) {
                             System.out.println(i.getFormattedMessage());
-                            exists = true;
+                            counter++;
                         }
                     }
-                    if (!exists)
+                    if (counter == 0) {
                         System.out.println("nothing found");
+                        log("SEARCH by keyword: " + command[2] + ", nothing found");
+                    } else {
+                        log("SEARCH by keyword: " + command[2] + ", found: " + counter);
+                    }
                     break;
                 case "-regex":
                     System.out.println("BY REGULAR EXPRESSION: " + command[2]);
-                    exists = false;
+                    counter = 0;
                     for (Message i : data) {
                         if (Pattern.matches(command[2], i.getMessage())) {
                             System.out.println(i.getFormattedMessage());
-                            exists = true;
+                            counter++;
                         }
                     }
-                    if (!exists)
+                    if (counter == 0) {
                         System.out.println("nothing found");
+                        log("SEARCH by regex: " + command[2] + ", nothing found");
+                    } else {
+                        log("SEARCH by regex: " + command[2] + ", found: " + counter);
+                    }
                     break;
             }
         } catch (Exception e) {
             System.out.println("Sorry, but something went wrong. Try to change the query.");
+            log("SEARCH failed");
         }
     }
 
+    private void log(String string) {
+        if (logfile == null)
+            return;
+        try {
+            logfile.write(LocalDateTime.now() + " " + string + "\n");
+        } catch (IOException e) {        }
+    }
+
     private ArrayList<Message> data;
+    private FileWriter logfile;
 
     private static int nextId = 0;
     private String file = "saved.txt";
